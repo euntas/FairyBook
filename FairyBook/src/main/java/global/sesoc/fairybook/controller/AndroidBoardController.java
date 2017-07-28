cb5b4401694db30a3c3a5e8f3d8123e866442390
jnlee1112 <jnlee1112@gmail.com>
2017-07-28 11:23:06 +0900

    write

--- FairyBook/src/main/java/global/sesoc/fairybook/controller/AndroidBoardController.java
+++ FairyBook/src/main/java/global/sesoc/fairybook/controller/AndroidBoardController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -59,6 +60,17 @@ public class AndroidBoardController {
 		String jsontext = jObject.toString();
 		System.out.println("jsontext: " + jsontext);
 		return jsontext;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "writeBoard", method = RequestMethod.POST)
+	public String writeBoard(@RequestBody String data) {
+
+			System.out.println("test : " +data);
+		
+	
+	
+		return "true";
 	}
 
 }
